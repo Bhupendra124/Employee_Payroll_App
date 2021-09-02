b89aa78b692f247611484d1bfb2a134dab3518ad
Bhupendra124 <wadekar.bhupendra6@gmail.com>
2021-09-02 18:39:32 +0530

    [bhupendra]ADD:change in fields

---
+++ JS/EmployeePayroll.js
@@ -0,0 +1,15 @@
+class EmployeePayrollData
+{
+    get id()
+    {
+        return this._id;
+    }
+    set id(id)
+    {
+        this._id = id;
+    }
+     
+    get name()
+    {
+        return this._name;
+    }
